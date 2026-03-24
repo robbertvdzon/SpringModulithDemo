7ac96bfef2d485ebe117b2dcc4a8a647ffe85403
Robbert vd Zon <robbert.vanderzon@edsn.nl>
2026-03-24 19:50:49 +0100

    demo

---
+++ src/test/kotlin/com/vdzon/springmodulithdemo/DeliveryIntegrationTest.kt
@@ -0,0 +1,15 @@
+package com.vdzon.springmodulithdemo
+
+import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.Test
+
+@SpringBootTest
+class DeliveryIntegrationTest {
+
+
+
+    @Test
+    fun test() {
+
+    }
+}
