d66826700142f3fcac448b8b7a7f2224f0729e57
Robbert vd Zon <robbert.vanderzon@edsn.nl>
2026-03-24 19:12:03 +0100

    demo

---
+++ src/main/kotlin/com/vdzon/springmodulithdemo/SpringModulithDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.vdzon.springmodulithdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringModulithDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringModulithDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/vdzon/springmodulithdemo/SpringModulithDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vdzon.springmodulithdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringModulithDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
